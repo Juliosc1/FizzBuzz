ff60d07ddc2756817003c99541d06b676fec9925
Juliosc1 <julio.siklander@gmail.com>
2022-03-30 12:39:37 +0200

    Add Selenium

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,41 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws InterruptedException
     {
-        System.out.println( "Hello World!" );
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver;
+        driver = new ChromeDriver();
+
+        driver.get("https://google.com");
+
+        System.out.println(driver.getTitle()); // => "Google"
+        System.out.println(driver.getCurrentUrl()); // => "Google"
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(5000));
+        Thread.sleep(5*1000);
+
+        WebElement searchBox = driver.findElement(By.name("q"));
+        WebElement searchButton = driver.findElement(By.name("btnK"));
+
+        //Skriv något i textrutan
+        searchBox.sendKeys("Selenium");
+        System.out.println(searchBox.getAttribute("value"));
+        searchButton.click();
+
+        searchBox = driver.findElement(By.name("q"));
+        searchBox.getAttribute("value"); // =>"Selenium
+
+        driver.quit();
     }
 }
